41db679ae766f8fcd56c7b3c3750a9f3beaed2ac
Arshad Chummun <arshad@chummun.com>
2011-09-18 02:03:05 +0400

    disabling chosen on field ui as it breaks core UX

--- chosen.js
+++ chosen.js
@@ -1,7 +1,10 @@
 (function($) {
   Drupal.behaviors.chosen = {
     attach: function(context) {
-      $(Drupal.settings.chosen.selector, context).add('.chosen-select').each(function() {
+      $(Drupal.settings.chosen.selector, context)
+        .add('.chosen-widget') //enable chosen for widgets
+        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
+        .each(function() {
         if ($(this).find('option').size() >= Drupal.settings.chosen.minimum) {
           $(this).chosen();
         }
