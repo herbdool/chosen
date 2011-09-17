41da8f2cb8e2028df368f72e853fa7f0b829284c
Arshad Chummun <arshad@chummun.com>
2011-09-18 02:09:18 +0400

    adding minimum width option

--- chosen.js
+++ chosen.js
@@ -1,12 +1,15 @@
 (function($) {
   Drupal.behaviors.chosen = {
     attach: function(context) {
+      var minWidth = Drupal.settings.chosen.minimum_width;
       $(Drupal.settings.chosen.selector, context)
         .add('.chosen-widget') //enable chosen for widgets
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
         .each(function() {
         if ($(this).find('option').size() >= Drupal.settings.chosen.minimum) {
-          $(this).chosen();
+          $(this).css({
+            width : ($(this).width() < minWidth) ? minWidth : $(this).width()
+          }).chosen();
         }
       }); 
     }
