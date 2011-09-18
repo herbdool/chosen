250001129a3c94e65ba4fc1f3c52618d5f8b5f4d
Arshad Chummun <arshad@chummun.com>
2011-09-18 17:13:18 +0400

    enable chosen for options_select widget instead of an standalone widget

--- chosen.js
+++ chosen.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.chosen = {
     attach: function(context) {
       var minWidth = Drupal.settings.chosen.minimum_width;
+      
       $(Drupal.settings.chosen.selector, context)
-        .add('.chosen-widget') //enable chosen for widgets
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
         .each(function() {
         if ($(this).find('option').size() >= Drupal.settings.chosen.minimum) {
@@ -11,7 +11,14 @@
             width : ($(this).width() < minWidth) ? minWidth : $(this).width()
           }).chosen();
         }
-      }); 
+      });
+       
+      //enable Chosen for widgets  
+      $('.chosen-widget', context).each(function() {
+        $(this).css({
+          width : ($(this).width() < minWidth) ? minWidth : $(this).width()
+        }).chosen();
+      });
     }
   }
 })(jQuery);
