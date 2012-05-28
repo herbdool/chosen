eb8816559199fed8baa80392ebc25ff0ec3a0b8a
git <git@1305230.no-reply.drupal.org>
2012-05-28 19:18:36 +0400

    add contains vs start with option [#1539444]

--- chosen.js
+++ chosen.js
@@ -2,14 +2,17 @@
   Drupal.behaviors.chosen = {
     attach: function(context) {
       var minWidth = Drupal.settings.chosen.minimum_width;
-      
+      //define options
+      var options = {};
+      options.search_contains = Drupal.settings.chosen.search_contains;
+
       $(Drupal.settings.chosen.selector, context)
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
         .each(function() {
         if ($(this).find('option').size() >= Drupal.settings.chosen.minimum) {
           $(this).css({
             width : ($(this).width() < minWidth) ? minWidth : $(this).width()
-          }).chosen();
+          }).chosen(options);
         }
       });
        
@@ -17,7 +20,7 @@
       $('.chosen-widget', context).each(function() {
         $(this).css({
           width : ($(this).width() < minWidth) ? minWidth : $(this).width()
-        }).chosen();
+        }).chosen(options);
       });
     }
   }
