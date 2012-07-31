60c042b30e19389dee513ce6c3f85fa119c5ed09
bago <bago@23711.no-reply.drupal.org>
2012-07-31 17:10:33 +0200

    Fixes #1591908 Default select text should be translatable

--- chosen.js
+++ chosen.js
@@ -5,6 +5,9 @@
       //define options
       var options = {};
       options.search_contains = Drupal.settings.chosen.search_contains;
+      options.placeholder_text_multiple = Drupal.t('Choose some options');
+      options.placeholder_text_single = Drupal.t('Choose an option');
+      options.no_results_text = Drupal.t('No results match');
 
       $(Drupal.settings.chosen.selector, context)
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
