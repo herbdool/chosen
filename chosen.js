55ba20f460367a2e425c4021503e544946c7302f
aheredia <aheredia@1253098.no-reply.drupal.org>
2013-07-09 12:08:40 -0500

    Issue #1609036 by aheredia: Use settings locally available to behaviors rather than Drupal.settings.

--- chosen.js
+++ chosen.js
@@ -1,25 +1,25 @@
 (function($) {
   Drupal.behaviors.chosen = {
-    attach: function(context) {
-      var minWidth = Drupal.settings.chosen.minimum_width;
+    attach: function(context, setings) {
+      var minWidth = settings.chosen.minimum_width;
       //define options
       var options = {};
-      options.search_contains = Drupal.settings.chosen.search_contains;
-      options.placeholder_text_multiple = Drupal.settings.chosen.placeholder_text_multiple;
-      options.placeholder_text_single = Drupal.settings.chosen.placeholder_text_single;
-      options.no_results_text = Drupal.settings.chosen.no_results_text;
+      options.search_contains = settings.chosen.search_contains;
+      options.placeholder_text_multiple = settings.chosen.placeholder_text_multiple;
+      options.placeholder_text_single = settings.chosen.placeholder_text_single;
+      options.no_results_text = settings.chosen.no_results_text;
 
-      $(Drupal.settings.chosen.selector, context)
+      $(settings.chosen.selector, context)
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
         .each(function() {
-        if ($(this).find('option').size() >= Drupal.settings.chosen.minimum) {
+        if ($(this).find('option').size() >= settings.chosen.minimum) {
           $(this).css({
             width : ($(this).width() < minWidth) ? minWidth : $(this).width()
           }).chosen(options);
         }
       });
-       
-      //enable Chosen for widgets  
+
+      //enable Chosen for widgets
       $('.chosen-widget', context).each(function() {
         $(this).css({
           width : ($(this).width() < minWidth) ? minWidth : $(this).width()
