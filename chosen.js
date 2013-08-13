202146a567b359895458d6960f011d813d40cb47
zambrey <zambrey@179474.no-reply.drupal.org>
2013-08-13 11:21:12 +0200

    Issue #2063511 by zambrey | HitmanInWis: Fixed Minimum number of options ignored.

--- chosen.js
+++ chosen.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.chosen = {
     attach: function(context, settings) {
       var minWidth = settings.chosen.minimum_width;
+      var minOptions = settings.chosen.minimum;
       //define options
       var options = {};
       options.search_contains = settings.chosen.search_contains;
@@ -12,10 +13,12 @@
       $(settings.chosen.selector, context)
         .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select') //disable chosen on field ui
         .each(function() {
-        options = $.extend(options, {
-          width: (($(this).width() < minWidth) ? minWidth : $(this).width()) + 'px'
-        });
-        $(this).chosen(options);
+          if ($(this).find('option').size() >= minOptions) {
+            options = $.extend(options, {
+              width: (($(this).width() < minWidth) ? minWidth : $(this).width()) + 'px'
+            });
+            $(this).chosen(options);
+          }
       });
 
       //enable Chosen for widgets
