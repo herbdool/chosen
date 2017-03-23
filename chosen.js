91bf5368295946c9c1b8ecb2032b813358da35f0
markcarver <markcarver@501638.no-reply.drupal.org>
2017-03-23 10:40:34 +0100

    Issue #2859781 by markcarver: Optimize JavaScript

--- chosen.js
+++ chosen.js
@@ -3,69 +3,160 @@
  * Attaches behaviors for the Chosen module.
  */
 
-(function($) {
+(function($, Drupal) {
+  'use strict';
+
+  // Update Chosen elements when state has changed.
+  // Note: This also handles jQuery 1.7+.
+  $(document)[$.fn.on ? 'on' : 'delegate']($.fn.on ? 'state:disabled' : 'select', $.fn.on ? 'select' : 'state:disabled', function (e) {
+    $(e.target).trigger('chosen:updated');
+  });
+
   Drupal.behaviors.chosen = {
+
+    settings: {
+
+      /**
+       * Completely ignores elements that match one of these selectors.
+       *
+       * Disabled on:
+       * - Field UI
+       * - WYSIWYG elements
+       * - Tabledrag weights
+       * - Elements that have opted-out of Chosen
+       * - Elements already processed by Chosen.
+       *
+       * @type {string}
+       */
+      ignoreSelector: '#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disable, .chosen-processed',
+
+      /**
+       * Explicit "opt-in" selector.
+       *
+       * @todo Remove support for the deprecated chosen-widget class.
+       *
+       * @type {string}
+       */
+      optedInSelector: 'select.chosen-enable, select.chosen-widget',
+
+      /**
+       * The default selector, can be overridden by Drupal.settings.
+       *
+       * @type {string}
+       */
+      selector: 'select:visible'
+    },
+
+    /**
+     * Drupal attach behavior.
+     */
     attach: function(context, settings) {
-      settings.chosen = settings.chosen || Drupal.settings.chosen;
+      this.settings = this.getSettings(settings);
+      this.getElements(context).once('chosen').each(function (i, element) {
+        this.createChosen(element);
+      }.bind(this));
+    },
 
-      // Prepare selector and add unwantend selectors.
-      var selector = settings.chosen.selector;
+    /**
+     * Creates a Chosen instance for a specific element.
+     *
+     * @param {jQuery|HTMLElement} element
+     *   The element.
+     */
+    createChosen: function(element) {
+      var $element = $(element);
+      $element.chosen(this.getElementOptions($element));
+    },
 
-      // Function to prepare all the options together for the chosen() call.
-      var getElementOptions = function (element) {
-        var options = $.extend({}, settings.chosen.options);
+    /**
+     * Filter out elements that should not be converted into Chosen.
+     *
+     * @param {jQuery|HTMLElement} element
+     *   The element.
+     *
+     * @return {boolean}
+     *   TRUE if the element should stay, FALSE otherwise.
+     */
+    filterElements: function (element) {
+      var $element = $(element);
 
-        // The width default option is considered the minimum width, so this
-        // must be evaluated for every option.
-        if (settings.chosen.minimum_width > 0) {
-          if ($(element).width() < settings.chosen.minimum_width) {
-            options.width = settings.chosen.minimum_width + 'px';
-          }
-          else {
-            options.width = $(element).width() + 'px';
-          }
+      // Remove elements that should be ignored completely.
+      if ($element.is(this.settings.ignoreSelector)) {
+        return false;
+      }
+
+      // Zero value means no minimum.
+      var minOptions = $element.attr('multiple') ? this.settings.minimum_multiple : this.settings.minimum_single;
+      return !minOptions || $element.find('option').length >= minOptions;
+    },
+
+    /**
+     * Retrieves the elements that should be converted into Chosen instances.
+     *
+     * @param {jQuery|Element} context
+     *   A DOM Element, Document, or jQuery object to use as context.
+     * @param {string} [selector]
+     *   A selector to use, defaults to the default selector in the settings.
+     */
+    getElements: function (context, selector) {
+      var $context = $(context || document);
+      var $elements = $context.find(selector || this.settings.selector);
+
+      // Remove elements that should not be converted into Chosen.
+      $elements = $elements.filter(function(i, element) {
+        return this.filterElements(element);
+      }.bind(this));
+
+      // Add elements that have explicitly opted in to Chosen.
+      $elements = $elements.add($context.find(this.settings.optedInSelector));
+
+      return $elements;
+    },
+
+    /**
+     * Retrieves options used to create a Chosen instance based on an element.
+     *
+     * @param {jQuery|HTMLElement} element
+     *   The element to process.
+     *
+     * @return {Object}
+     *   The options object used to instantiate a Chosen instance with.
+     */
+    getElementOptions: function (element) {
+      var $element = $(element);
+      var options = $.extend({}, this.settings.options);
+
+      // The width default option is considered the minimum width, so this
+      // must be evaluated for every option.
+      if (this.settings.minimum_width > 0) {
+        if ($element.width() < this.settings.minimum_width) {
+          options.width = this.settings.minimum_width + 'px';
         }
-
-        // Some field widgets have cardinality, so we must respect that.
-        // @see chosen_pre_render_select()
-        if ($(element).attr('multiple') && $(element).data('cardinality')) {
-          options.max_selected_options = $(element).data('cardinality');
+        else {
+          options.width = $element.width() + 'px';
         }
+      }
 
-        return options;
-      };
+      // Some field widgets have cardinality, so we must respect that.
+      // @see chosen_pre_render_select()
+      var cardinality;
+      if ($element.attr('multiple') && (cardinality = $element.data('cardinality'))) {
+        options.max_selected_options = cardinality;
+      }
 
-      // Process elements that have opted-in for Chosen.
-      // @todo Remove support for the deprecated chosen-widget class.
-      $('select.chosen-enable, select.chosen-widget', context).once('chosen', function() {
-        options = getElementOptions(this);
-        $(this).chosen(options);
-      });
+      return options;
+    },
 
-      $(selector, context)
-        // Disabled on:
-        // - Field UI
-        // - WYSIWYG elements
-        // - Tabledrag weights
-        // - Elements that have opted-out of Chosen
-        // - Elements already processed by Chosen.
-        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disable, .chosen-processed')
-        .filter(function() {
-          // Filter out select widgets that do not meet the minimum number of
-          // options.
-          var minOptions = $(this).attr('multiple') ? settings.chosen.minimum_multiple : settings.chosen.minimum_single;
-          if (!minOptions) {
-            // Zero value means no minimum.
-            return true;
-          }
-          else {
-            return $(this).find('option').length >= minOptions;
-          }
-        })
-        .once('chosen', function() {
-          options = getElementOptions(this);
-          $(this).chosen(options);
-        });
+    /**
+     * Retrieves the settings passed from Drupal.
+     *
+     * @param {Object} [settings]
+     *   Passed Drupal settings object, if any.
+     */
+    getSettings: function (settings) {
+      return $.extend(true, {}, this.settings, settings && settings.chosen || Drupal.settings && Drupal.settings.chosen);
     }
+
   };
-})(jQuery);
+
+})(jQuery, Drupal);
